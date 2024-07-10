1edff3a13a73417595945fafd16b7287b389532a
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-07-10 11:25:30 -0500

    ok for real, this is it

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,50 +1,57 @@
 #!/usr/bin/python3
-''' This script will gather data from an employee ID and returns
-    information about his/her list progress '''
+"""
+API set up to retrieve data from a url
+"""
 import requests
-import sys
+
+API_URL = "https://jsonplaceholder.typicode.com/users"
+TODOS_URL = "https://jsonplaceholder.typicode.com/todos"
 
 
-def get_employee_name(employee_id):
-    ''' This function will return the name of the employee '''
-    url = "{}/{}".format(base_url, employee_id)
-    response = requests.get(url)
-    return response.json().get("name")
+def get_employee_info(employee_id):
+    # Get employee information from the API
+    employee_response = requests.get(f"{API_URL}/{employee_id}")
+    employee_data = employee_response.json()
+
+    # Get TODO list for the employee
+    todos_response = requests.get(TODOS_URL,
+                                  params={"userId": employee_id})
+    todos_data = todos_response.json()
+
+    # Extract completed tasks and count
+    completed_tasks = [todo["title"]
+                       for todo in todos_data if todo["completed"]]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos_data)
+
+    return (employee_data["name"],
+            num_completed_tasks,
+            total_tasks,
+            completed_tasks)
 
 
-def get_assigned_tasks(employee_id):
-    ''' This function will return the number of all tasks
-        assigned to that the employee '''
-    url = "{}/{}/todos".format(base_url, employee_id)
-    response = requests.get(url)
-    return len(response.json())
-
-
-def get_completed_tasks(employee_id):
-    ''' This function will return the number of tasks
-        that the employee has completed '''
-    finished_tasks = []
-    url = "{}/{}/todos".format(base_url, employee_id)
-    response = requests.get(url)
-    for task in response.json():
-        if task.get("completed"):
-            finished_tasks.append(task.get("title"))
-    return finished_tasks
-
-
-def print_employee_status(employee_name, completed_tasks, assigned_tasks):
-    ''' This function will return the information about the employee'''
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                          len(completed_tasks),
-                                                          assigned_tasks))
+def display_employee_progress(employee_name,
+                              num_completed_tasks,
+                              total_tasks,
+                              completed_tasks):
+    print(
+        "Employee {} is done with tasks({}/{}):"
+        .format(employee_name, num_completed_tasks, total_tasks)
+    )
     for task in completed_tasks:
-        print("\t {}".format(task))
+        print("\t", task)
 
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com/users"
-    employee_name = get_employee_name(employee_id)
-    assigned_tasks = get_assigned_tasks(employee_id)
-    completed_tasks = get_completed_tasks(employee_id)
-    print_employee_status(employee_name, completed_tasks, assigned_tasks)
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    try:
+        name, completed, total, tasks = get_employee_info(employee_id)
+        display_employee_progress(name, completed, total, tasks)
+    except requests.exceptions.RequestException:
+        print("Error: Unable to fetch data from the API.")
